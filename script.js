2fe61c03cdd210d09c15b86a1e9af363ee7e23c2
oyinade <adepejuoyinade@gmail.com>
2022-05-17 15:26:00 -0700

    second commit

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 const hamburger = document.querySelector(".hamburger");
 const navItems = document.querySelector(".nav-items");
 const navIcon = document.querySelectorAll(".navIcon");
-const menu = document.querySelector("#menu");
+
 
 hamburger.addEventListener("click", () => {
   navItems.classList.toggle("open");
-  navIcon.forEach((icon) => {
-    icon.classList.toggle("hidden");
-  });
+  // navIcon.forEach((icon) => {
+  //   icon.classList.toggle("hidden");
+  // });
 });
 
+document.querySelectorAll("li").forEach((n) =>
+  n.addEventListener("click", () => {
+    navItems.classList.remove("open");
+    
+  })
+);
