3c9f2df87509fcf01399c1899ab7b70aa92e753e
oyinade <adepejuoyinade@gmail.com>
2022-05-17 16:35:54 -0700

    another commit

--- script.js
+++ script.js
@@ -2,17 +2,12 @@ const hamburger = document.querySelector(".hamburger");
 const navItems = document.querySelector(".nav-items");
 const navIcon = document.querySelectorAll(".navIcon");
 
-
 hamburger.addEventListener("click", () => {
   navItems.classList.toggle("open");
-  // navIcon.forEach((icon) => {
-  //   icon.classList.toggle("hidden");
-  // });
 });
 
 document.querySelectorAll("li").forEach((n) =>
   n.addEventListener("click", () => {
     navItems.classList.remove("open");
-    
   })
 );
